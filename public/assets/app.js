64d904f96a018d75a4a25c6c5e7a3b82a5c27a5c
Anthony James Manenti <anthonyj.manenti@gmail.com>
2018-07-03 21:03:52 -0400

    updates before deadline

--- public/assets/app.js
+++ public/assets/app.js
@@ -5,9 +5,14 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p><br>");
+    $("#articles").append("<a class='btn btn-primary' href='" + data[i].link + "' role='button'>Read Article</a><HR>");
   }
 });
+
+
+
+
 
 
 // Whenever someone clicks a p tag
@@ -28,7 +33,7 @@ $(document).on("click", "p", function() {
       // The title of the article
       $("#notes").append("<h4>" + data.title + "</h4>");
       // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' placeholder='enter title here...'><br>");
+      $("#notes").append("<input id='titleinput' name='title' placeholder='enter title here...'><hr>");
       // A textarea to add a new note body
       $("#notes").append("<textarea id='bodyinput' name='body' placeholder='enter a note...'></textarea><br>");
       // A button to submit a new note, with the id of the article saved to it
